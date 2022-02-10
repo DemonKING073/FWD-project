a44ead9c370e670a4f8c8a283f44ebf30a956804
Din Bibek Thapa <legendsnever7die@gmail.com>
2022-02-10 14:30:55 +0545

    responsive added to gallery

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var counter = 1;
 setInterval(function () {
   console.log(counter);
+  if (counter == 1) {
+    document.getElementById(`radio4`).checked = false;
+  }
   document.getElementById(`radio${counter}`).checked = true;
   if (counter > 1)
     document.getElementById(`radio${counter - 1}`).checked = false;
   counter++;
   if (counter > 4) {
     counter = 1;
-    document.getElementById(`radio4`).checked = false;
   }
-}, 2000);
+}, 5000);
